e1283928d9a7c9388611f1b6e6199c7715979927
Nick Wright <nwright98@gmail.com>
2018-07-20 17:51:01 -0400

    Updating examples

--- examples/c/mcc118/CodeBlocks/DataLogger/log_file.h
+++ examples/c/mcc118/CodeBlocks/DataLogger/log_file.h
@@ -7,6 +7,7 @@ extern FILE* log_file_ptr;
 
 extern char* choose_log_file(GtkWidget *parent_window, char* default_file_name);
 extern FILE* open_log_file (char* filename);
-extern int write_log_file(FILE* log_file_ptr, double* read_buf, int numberOfSamplesPerChannel, int numberOfChannels);
+extern int write_log_file(FILE* log_file_ptr, double* read_buf, 
+    int numberOfSamplesPerChannel, int numberOfChannels);
 
 #endif // LOG_FILE_H_INCLUDED
--- examples/c/mcc118/finite_scan_with_trigger/finite_scan_with_trigger.c
+++ examples/c/mcc118/finite_scan_with_trigger/finite_scan_with_trigger.c
@@ -1,18 +1,20 @@
 /*****************************************************************************
 
     MCC 118 Functions Demonstrated:
+        mcc118_trigger_mode
         mcc118_a_in_scan_start
+        mcc118_a_in_scan_status
         mcc118_a_in_scan_read
-        mcc118_a_in_scan_stop
 
     Purpose:
-        Perform a continuous acquisition on 1 or more channels.
+        Perform a triggered finite acquisition on 1 or more channels.
 
     Description:
-        Continuously acquires blocks of analog input data for a
-        user-specified group of channels until the acquisition is
-        stopped by the user.  The last sample of data for each channel
-        is displayed for each block of data received from the device.
+        Waits for an external trigger to occur and then acquires blocks
+        of analog input data for a user-specified group of channels.  The
+        last sample of data for each channel is displayed for each block
+        of data received from the device.  The acquisition is stopped when
+        the specified number of samples is acquired for each channel.
 
 *****************************************************************************/
 #include "../../daqhats_utils.h"
@@ -21,47 +23,44 @@ int main(void)
 {
     int result = RESULT_SUCCESS;
     uint8_t address = 0;
-    char c;
-    char display_header[512];
-    int i;
     char channel_string[512];
     char options_str[512];
+    char c;
+    char display_header[512];
+    char trigger_mode_str[512];
+    int i;
 
     // Set the channel mask which is used by the library function
     // mcc118_a_in_scan_start to specify the channels to acquire.
     // The functions below, will parse the channel mask into a
     // character string for display purposes.
     uint8_t channel_mask = {CHAN0 | CHAN1 | CHAN2 | CHAN3};
-    convert_chan_mask_to_string(channel_mask, channel_string);
-    
-    uint32_t samples_per_channel = 0;
+    convert_chan_mask_to_string(channel_mask, channel_string) ;
 
     int max_channel_array_length = mcc118_a_in_num_channels();
     int channel_array[max_channel_array_length];
-    uint8_t num_channels = convert_chan_mask_to_array(channel_mask, 
+    uint8_t num_channels = convert_chan_mask_to_array(channel_mask,
         channel_array);
 
-    uint32_t internal_buffer_size_samples = 0;
-    uint32_t user_buffer_size = 1000 * num_channels;
-    double read_buf[user_buffer_size];
+    uint32_t samples_per_channel = 10000;
+    uint32_t buffer_size = samples_per_channel * num_channels;
+    double read_buf[buffer_size];
     int total_samples_read = 0;
 
-    int32_t read_request_size = READ_ALL_AVAILABLE;
-
-    // When doing a continuous scan, the timeout value will be ignored in the
-    // call to mcc118_a_in_scan_read because we will be requesting that all
-    // available samples (up to the default buffer size) be returned.
+    int32_t read_request_size = 500;
     double timeout = 5.0;
 
     double scan_rate = 1000.0;
     double actual_scan_rate = 0.0;
     mcc118_a_in_scan_actual_rate(num_channels, scan_rate, &actual_scan_rate);
 
-    uint32_t options = OPTS_CONTINUOUS;
+    uint32_t options = OPTS_EXTTRIGGER;
 
     uint16_t read_status = 0;
     uint32_t samples_read_per_channel = 0;
 
+    uint8_t trigger_mode = TRIG_RISING_EDGE;
+    int cancel_trigger = 0;
 
     // Select an MCC118 HAT device to use.
     if (select_hat_device(HAT_ID_MCC_118, &address))
@@ -70,41 +69,57 @@ int main(void)
         return -1;
     }
 
-    printf ("\nSelected MCC 118 device at address %d\n", address);
+    printf("\nSelected MCC 118 device at address %d\n", address);
 
     // Open a connection to the device.
     result = mcc118_open(address);
     STOP_ON_ERROR(result);
 
     convert_options_to_string(options, options_str);
-    convert_chan_mask_to_string(channel_mask, channel_string) ;
+    convert_trigger_mode_to_string(trigger_mode, trigger_mode_str);
 
-    printf("\nMCC 118 continuous scan example\n");
+    printf("\nMCC118 finite scan with trigger  example\n");
     printf("    Functions demonstrated:\n");
+    printf("        mcc118_trigger_mode\n");
     printf("        mcc118_a_in_scan_start\n");
+    printf("        mcc118_a_in_scan_status\n");
     printf("        mcc118_a_in_scan_read\n");
-    printf("        mcc118_a_in_scan_stop\n");
     printf("    Channels: %s\n", channel_string);
+    printf("    Samples per channel: %d\n", samples_per_channel);
     printf("    Requested scan rate: %-10.2f\n", scan_rate);
     printf("    Actual scan rate: %-10.2f\n", actual_scan_rate);
     printf("    Options: %s\n", options_str);
+    printf("    Trigger mode: %s\n", trigger_mode_str);
 
-    printf("\nPress ENTER to continue ...\n");
+    printf("\nPress ENTER to continue\n");
     scanf("%c", &c);
 
+    result = mcc118_trigger_mode(address, trigger_mode);
+    STOP_ON_ERROR(result);
+
     // Configure and start the scan.
-    // Since the continuous option is being used, the samples_per_channel 
-    // parameter is ignored if the value is less than the default internal
-    // buffer size (10000 * num_channels in this case). If a larger internal
-    // buffer size is desired, set the value of this parameter accordingly.
     result = mcc118_a_in_scan_start(address, channel_mask, samples_per_channel,
         scan_rate, options);
     STOP_ON_ERROR(result);
 
-    STOP_ON_ERROR(mcc118_a_in_scan_buffer_size(address,
-        &internal_buffer_size_samples));
-    printf("Internal data buffer size:  %d\n", internal_buffer_size_samples);
+    printf("Waiting for trigger ... hit ENTER to cancel the trigger\n");
+    do
+    {
+        result = mcc118_a_in_scan_status(address, &read_status,
+            &samples_read_per_channel);
+        STOP_ON_ERROR(result);
 
+        cancel_trigger = enter_press();
+    } 
+    while ((result == RESULT_SUCCESS) &&
+           ((read_status & STATUS_TRIGGERED) != STATUS_TRIGGERED) && 
+           !cancel_trigger);
+
+    if (cancel_trigger)
+    {
+        printf("Trigger cancelled by user\n");
+        goto stop;
+    }
 
     printf("\nStarting scan ... Press ENTER to stop\n\n");
 
@@ -116,16 +131,16 @@ int main(void)
         strcat(display_header, channel_string);
     }
     strcat(display_header, "\n");
+
     printf("%s", display_header);
 
     // Continuously update the display value until enter key is pressed
-    do 
+    // or the number of samples requested has been read.
+    do
     {
-        // Since the read_request_size is set to -1 (READ_ALL_AVAILABLE), this
-        // function returns immediately with whatever samples are available (up
-        // to user_buffer_size) and the timeout parameter is ignored.
+        // Read the specified number of samples.
         result = mcc118_a_in_scan_read(address, &read_status, read_request_size,
-            timeout, read_buf, user_buffer_size, &samples_read_per_channel);
+            timeout, read_buf, buffer_size, &samples_read_per_channel);
         STOP_ON_ERROR(result);
         if (read_status & STATUS_HW_OVERRUN)
         {
@@ -153,20 +168,17 @@ int main(void)
             }
             fflush(stdout);
         }
-
-        usleep(500000);
-
     } 
-    while ((result == RESULT_SUCCESS) &&
+    while ((result == RESULT_SUCCESS) && 
            ((read_status & STATUS_RUNNING) == STATUS_RUNNING) && 
            !enter_press());
 
     printf("\n");
 
 stop:
-    print_error(mcc118_a_in_scan_stop(address));
-    print_error(mcc118_a_in_scan_cleanup(address));
-    print_error(mcc118_close(address));
+    mcc118_a_in_scan_stop(address);
+    mcc118_a_in_scan_cleanup(address);
+    mcc118_close(address);
 
     return 0;
 }
