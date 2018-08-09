7a9ade3841673a685c20ec57d2d62f6a2d4ab6b4
Nick Wright <nwright98@gmail.com>
2018-08-09 17:14:19 -0400

    Updating docs

--- docs/_static/documentation_options.js
+++ docs/_static/documentation_options.js
@@ -1,10 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: '',
-<<<<<<< HEAD
     VERSION: '0.3.0',
-=======
-    VERSION: '1.0.0',
->>>>>>> dev
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
